526b08842b6ece61bf77e5a9dda1f40c4f56a49a
SuperMcBrent <brentsagaert@gmail.com>
2023-09-11 23:03:06 +0200

    wello horld

--- scripts/components.js
+++ scripts/components.js
@@ -15,7 +15,9 @@
         dropdown: createRow_Select,
         header: createRow_Header,
         checkbox: createRow_Checkbox,
-        segment: createRow_Segment
+        segment: createRow_Segment,
+        progress: createRow_Progress,
+        chart: createRow_Chart
     };
 
     function initialise() {
@@ -27,7 +29,7 @@
     }
 
     async function addComponent(blueprint) {
-        if($(blueprint.dependsOn).length) {
+        if ($(blueprint.dependsOn).length) {
             actualAddComponent(blueprint);
             return;
         }
@@ -56,15 +58,15 @@
     }
 
     function createTab(blueprint) {
-        if(!blueprint.selectedTabIndex) {
+        if (!blueprint.selectedTabIndex) {
             blueprint.selectedTabIndex = 0;
         }
-        if(blueprint.tabs.length === 1) {
+        if (blueprint.tabs.length === 1) {
             return;
         }
         const tabContainer = $('<div/>').addClass('tabs');
         blueprint.tabs.forEach((element, index) => {
-            if(element.hidden) {
+            if (element.hidden) {
                 return;
             }
             const tab = $('<button/>')
@@ -72,10 +74,10 @@
                 .addClass('tabButton')
                 .text(element.title)
                 .click(changeTab.bind(null, blueprint, index));
-            if(blueprint.selectedTabIndex !== index) {
+            if (blueprint.selectedTabIndex !== index) {
                 tab.addClass('tabButtonInactive')
             }
-            if(index !== 0) {
+            if (index !== 0) {
                 tab.addClass('lineLeft')
             }
             tabContainer.append(tab);
@@ -84,11 +86,11 @@
     }
 
     function createRow(rowBlueprint) {
-        if(!rowTypeMappings[rowBlueprint.type]) {
+        if (!rowTypeMappings[rowBlueprint.type]) {
             console.warn(`Skipping unknown row type in blueprint: ${rowBlueprint.type}`, rowBlueprint);
             return;
         }
-        if(rowBlueprint.hidden) {
+        if (rowBlueprint.hidden) {
             return;
         }
         return rowTypeMappings[rowBlueprint.type](rowBlueprint);
@@ -96,10 +98,10 @@
 
     function createRow_Item(itemBlueprint) {
         const parentRow = $('<div/>').addClass('customRow');
-        if(itemBlueprint.image) {
+        if (itemBlueprint.image) {
             parentRow.append(createImage(itemBlueprint));
         }
-        if(itemBlueprint?.name) {
+        if (itemBlueprint?.name) {
             parentRow
                 .append(
                     $('<div/>')
@@ -113,7 +115,7 @@
                     .addClass('myItemValue')
                     .text(itemBlueprint?.extra ?? '')
             );
-        if(itemBlueprint?.value) {
+        if (itemBlueprint?.value) {
             parentRow
                 .append(
                     $('<div/>')
@@ -126,7 +128,7 @@
 
     function createRow_Input(inputBlueprint) {
         const parentRow = $('<div/>').addClass('customRow myItemInputRowAdjustment');
-        if(inputBlueprint.text) {
+        if (inputBlueprint.text) {
             parentRow
                 .append(
                     $("<div/>")
@@ -161,7 +163,7 @@
 
     function createRow_Button(buttonBlueprint) {
         const parentRow = $('<div/>').addClass('customRow myItemInputRowAdjustment');
-        for(const button of buttonBlueprint.buttons) {
+        for (const button of buttonBlueprint.buttons) {
             parentRow
                 .append(
                     $(`<button class='myButton'>${button.text}</button>`)
@@ -183,7 +185,7 @@
             .change(inputDelay(function (e) {
                 selectBlueprint.action(this.value);
             }, selectBlueprint.delay || 0));
-        for(const option of selectBlueprint.options) {
+        for (const option of selectBlueprint.options) {
             select.append(`<option value='${option.value}' ${option.selected ? 'selected' : ''}>${option.text}</option>`);
         }
         parentRow.append(select);
@@ -194,15 +196,15 @@
         const parentRow =
             $('<div/>')
                 .addClass('myHeader lineTop')
-        if(headerBlueprint.image) {
-            parentRow.append(createImage(headerBlueprint));
+        if (headerBlueprint.image) {
+            parentRow.append(createImage(itemBlueprint));
         }
         parentRow.append(
             $('<div/>')
                 .addClass('myName')
                 .text(headerBlueprint.title)
         )
-        if(headerBlueprint.action) {
+        if (headerBlueprint.action) {
             parentRow
                 .append(
                     $('<button/>')
@@ -212,8 +214,16 @@
                         .css('background-color', colorMapper(headerBlueprint.color || 'success'))
                         .click(headerBlueprint.action)
                 )
+        } else if (headerBlueprint.textRight) {
+            parentRow.append(
+                $('<div/>')
+                    .addClass('level')
+                    .text(headerBlueprint.title)
+                    .css("margin-left", "auto")
+                    .html(headerBlueprint.textRight)
+            )
         }
-        if(headerBlueprint.centered) {
+        if (headerBlueprint.centered) {
             parentRow.css('justify-content', 'center');
         }
         return parentRow;
@@ -246,12 +256,49 @@
     }
 
     function createRow_Segment(segmentBlueprint) {
-        if(segmentBlueprint.hidden) {
+        if (segmentBlueprint.hidden) {
             return;
         }
         return segmentBlueprint.rows.flatMap(createRow);
     }
 
+    function createRow_Progress(progressBlueprint) {
+        const parentRow = $('<div/>').addClass('customRow');
+        const up = progressBlueprint.numerator;
+        const down = progressBlueprint.denominator;
+        parentRow.append(
+            $('<div/>')
+                .addClass('myBar')
+                .append(
+                    $('<div/>')
+                        .css("height", "100%")
+                        .css("width", progressBlueprint.progressPercent + "%")
+                        .css("background-color", colorMapper(progressBlueprint.color || "rgb(122, 118, 118)"))
+                )
+        );
+        parentRow.append(
+            $('<div/>')
+                .addClass('myPercent')
+                .text(progressBlueprint.progressPercent + "%")
+        )
+        parentRow.append(
+            $('<div/>')
+                .css("margin-left", "auto")
+                .text(progressBlueprint.progressText)
+        )
+        return parentRow;
+    }
+
+    function createRow_Chart(chartBlueprint) {
+        const parentRow = $('<div/>')
+        .addClass('lineTop')
+            .append(
+                $("<canvas/>")
+                    .attr('id', chartBlueprint.chartId)
+            );
+        return parentRow;
+    }
+    
     function createImage(blueprint) {
         return $('<div/>')
             .addClass('myItemImage image')
@@ -270,7 +317,7 @@
 
     function addStyles() {
         const head = document.getElementsByTagName('head')[0]
-        if(!head) { return; }
+        if (!head) { return; }
         const style = document.createElement('style');
         style.type = 'text/css';
         style.innerHTML = styles;
@@ -289,10 +336,10 @@
     }
 
     function search(blueprint, query) {
-        if(!blueprint.idMappings) {
+        if (!blueprint.idMappings) {
             generateIdMappings(blueprint);
         }
-        if(!blueprint.idMappings[query]) {
+        if (!blueprint.idMappings[query]) {
             throw `Could not find id ${query} in blueprint ${blueprint.componentId}`;
         }
         return blueprint.idMappings[query];
@@ -300,30 +347,30 @@
 
     function generateIdMappings(blueprint) {
         blueprint.idMappings = {};
-        for(const tab of blueprint.tabs) {
+        for (const tab of blueprint.tabs) {
             addIdMapping(blueprint, tab);
-            for(const row of tab.rows) {
+            for (const row of tab.rows) {
                 addIdMapping(blueprint, row);
             }
         }
     }
 
     function addIdMapping(blueprint, element) {
-        if(element.id) {
-            if(blueprint.idMappings[element.id]) {
+        if (element.id) {
+            if (blueprint.idMappings[element.id]) {
                 throw `Detected duplicate id ${element.id} in blueprint ${blueprint.componentId}`;
             }
             blueprint.idMappings[element.id] = element;
         }
         let subelements = null;
-        if(element.type === 'segment') {
+        if (element.type === 'segment') {
             subelements = element.rows;
         }
-        if(element.type === 'buttons') {
+        if (element.type === 'buttons') {
             subelements = element.buttons;
         }
-        if(subelements) {
-            for(const subelement of subelements) {
+        if (subelements) {
+            for (const subelement of subelements) {
                 addIdMapping(blueprint, subelement);
             }
         }
@@ -523,10 +570,22 @@
         .customCheckBoxDisabled {
             color: #aaa
         }
+        .myBar {
+            height: 12px;
+            flex: 1;
+            background-color: #ffffff0a;
+            overflow: hidden;
+            max-width: 50%;
+            border-radius: 999px;
+        }
+        .myPercent {
+            margin-left: var(--margin);
+            margin-right: var(--margin);
+            color: #aaa;
+        }
     `;
 
     initialise();
 
     return exports;
-
 }
---
+++ scripts/skillOverviewPage.js
@@ -0,0 +1,207 @@
+(pages, components, elementWatcher, easyRequests) => {
+
+    let levelsAndXp = [];
+    let skills = [];
+    let uncollapsedSkill = undefined;
+    let lastChart = undefined;
+
+    async function initialise() {
+        pages.registerPage(pageBlueprint, handlePage);
+        levelsAndXp = await easyRequests.getLevelsAndXp();
+        skills = await easyRequests.getSkills();
+
+        skills = skills.filter(function( obj ) {
+            return obj.name !== 'TotalExp' && obj.name !== 'TotalLevel';
+        });
+
+        //console.log(skills);
+        levelsAndXp = levelsAndXp.map(obj => ({ ...obj, maxLevel: 100, showXP: true, showLvl: true }))
+        //console.log(levelsAndXp);
+
+        const totalLevel = levelsAndXp.reduce(function (sum, skill) {
+            return sum + skill.level;
+        }, 0);
+        //console.log(totalLevel)
+        const totalPossibleLevel = levelsAndXp.length * 100;
+        //console.log(totalPossibleLevel)
+
+        const totalXp = levelsAndXp.reduce(function (sum, skill) {
+            return sum + skill.totalExp;
+        }, 0);
+        console.log(totalXp)
+        const totalPossibleXp = levelsAndXp.length * 12000000;
+        console.log(totalPossibleXp)
+
+        levelsAndXp.push({
+            skill: "TotalLevel",
+            level: totalLevel,
+            exp: totalLevel,
+            maxLevel: totalPossibleLevel,
+            expToLevel: totalPossibleLevel - totalLevel,
+            showXP: false,
+            showLvl: true
+        });
+
+        levelsAndXp.push({
+            skill: "TotalExp",
+            level: totalXp,
+            exp: totalXp,
+            maxLevel: totalPossibleXp,
+            expToLevel: totalPossibleXp - totalXp,
+            showXP: true,
+            showLvl: false
+        });
+
+        // console.log(levelsAndXp);
+    }
+
+    async function handlePage() {
+        await update();
+    }
+
+    function clear() {
+
+    }
+
+    async function update() {
+        clear();
+        await elementWatcher.exists(componentBlueprint.dependsOn);
+
+        let column = 0;
+
+        levelsAndXp.forEach(async skilldata => {
+            componentBlueprint.componentId = "skillOverviewComponent_" + skilldata.skill;
+            componentBlueprint.parent = ".column" + column;
+            column === 0 ? column = 1 : column = 0;
+
+            const skillNameImageFix = {
+                "OneHanded": "one-handed",
+                "TwoHanded": "two-handed",
+                "TotalExp": "rank-one",
+                "TotalLevel": "rank-one",
+            }
+            let skillIcon = skillNameImageFix[skilldata.skill];
+            if (!skillIcon) skillIcon = skilldata.skill.toLowerCase(); // lowercase important!
+
+            const skillHeader = components.search(componentBlueprint, 'skillHeader');
+            skillHeader.title = skilldata.skill;
+            skillHeader.image = `/assets/misc/${skillIcon}.png`;
+            if (skilldata.showLvl) {
+                skillHeader.textRight = `Lv. ${skilldata.level} <span style='color: #aaa'>/ ${skilldata.maxLevel}</span>`;
+            } else {
+                skillHeader.textRight = "";
+            }
+
+
+            const skillProgress = components.search(componentBlueprint, 'skillProgress');
+            if (skilldata.showXP) {
+                skillProgress.progressText = `${skilldata.exp.toLocaleString("en-US")} / ${(skilldata.exp + skilldata.expToLevel).toLocaleString("en-US")} XP`;
+            } else {
+                skillProgress.progressText = "";
+            }
+            skillProgress.progressPercent = Math.trunc(skilldata.exp / (skilldata.exp + skilldata.expToLevel) * 100);
+            skillProgress.color = skills.find(s => s.name === skilldata.skill)?.color || null;
+
+            // const skillGraphHeader = components.search(componentBlueprint, 'skillGraphHeader');
+            // skillGraphHeader.title = `${skilldata.skill} Progress Graph`;
+            // skillGraphHeader.action = () => { toggleCollapsedState(skilldata.skill); }
+            // if (skilldata.skill === uncollapsedSkill) {
+            //     skillGraphHeader.name = "⬆️ Hide graph";
+            // } else {
+            //     skillGraphHeader.name = "⬇️ Show graph";
+            // }
+
+            // if (skilldata.skill === uncollapsedSkill) {
+            //     componentBlueprint.tabs[0].rows.push({
+            //         "id": "skillChart",
+            //         "type": "chart",
+            //         "chartId": "myChart"
+            //     })
+            // } else {
+            //     componentBlueprint.tabs[0].rows.length = 3;
+            // }
+
+            components.removeComponent(componentBlueprint);
+            components.addComponent(componentBlueprint);
+
+            // await elementWatcher.exists("#myChart");
+
+            // if (lastChart) lastChart.destroy();
+            // lastChart = new Chart($("#myChart"), {
+            //     type: 'line',
+            //     data: {
+            //         labels: ["Teets", "Nibbo", "Brent", "Panks", "Eenie", "Miny", "Mo?"],
+            //         datasets: [{
+            //             label: 'Some line graph :)',
+            //             data: [65, 59, 80, 81, 56, 55, 40],
+            //             fill: false,
+            //             borderColor: 'rgb(75, 192, 192)',
+            //             tension: 0.1
+            //         }]
+            //     },
+            //     // options: {
+            //     //     scales: {
+            //     //         y: {
+            //     //             beginAtZero: true
+            //     //         }
+            //     //     }
+            //     // }
+            // });
+
+        });
+    }
+
+    function toggleCollapsedState(skillname) {
+        if (uncollapsedSkill === skillname) {
+            uncollapsedSkill = undefined;
+        } else {
+            uncollapsedSkill = skillname;
+        }
+        update();
+    }
+
+    const pageBlueprint = {
+        "category": "Skills",
+        "pageName": "Overview",
+        "pageImage": "https://cdn-icons-png.flaticon.com/128/1160/1160329.png",
+        "columns": "2",
+        "onVisit": () => { }
+    }
+
+    const componentBlueprint = {
+        "componentId": "skillOverviewComponent",
+        "dependsOn": "custom-page",
+        "parent": ".column0",
+        "selectedTabIndex": 0,
+        "tabs": [
+            {
+                "title": "Skillname",
+                "rows": [
+                    {
+                        "id": "skillHeader",
+                        "type": "header",
+                        "title": "Forging",
+                        "image": "/assets/misc/merchant.png",
+                        "textRight": "Lv. 69 <span style='color: #aaa'>/ 420</span>"
+                    },
+                    {
+                        "id": "skillProgress",
+                        "type": "progress",
+                        "progressText": "301,313 / 309,469 XP",
+                        "progressPercent": "97"
+                    },
+                    // {
+                    //     "id": "skillGraphHeader",
+                    //     "type": "header",
+                    //     "title": "Skill Progress Graph",
+                    //     "name": "⬇️ Show graph",
+                    //     "color": "inverse",
+                    //     "action": () => { console.log("Header Action!"); }
+                    // },
+                ]
+            },
+        ]
+    };
+
+    initialise();
+}
