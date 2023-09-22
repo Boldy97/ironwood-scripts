e006ac2c1a3eac189df0f1fd067559464306883b
Boldy97 <robin.de.bruin@hotmail.com>
2023-09-22 23:05:31 +0200

    Inject a bit later for firefox

--- plugin.js
+++ plugin.js
@@ -1,19 +1,19 @@
 // ==UserScript==
 // @name         Ironwood RPG - Pancake-Scripts
 // @namespace    http://tampermonkey.net/
-// @version      2.8
+// @version      2.9
 // @description  A collection of scripts to enhance Ironwood RPG - https://github.com/Boldy97/ironwood-scripts
 // @author       Pancake
 // @match        https://ironwoodrpg.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ironwoodrpg.com
 // @grant        none
-// @run-at       document-start
+// @run-at       document-body
 // @require      https://code.jquery.com/jquery-3.6.4.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.js
 // ==/UserScript==
 
 window.PANCAKE_ROOT = 'https://iwrpg.vectordungeon.com';
-window.PANCAKE_VERSION = '2.8';
+window.PANCAKE_VERSION = '2.9';
 (() => {
 
     if(window.moduleRegistry) {
