633ad78658cae5a8505adf0761c06b192f56b20e
Boldy97 <robin.de.bruin@hotmail.com>
2024-01-13 22:58:54 +0100

    Actually set the version to 4.0

--- packager/prefix.js
+++ packager/prefix.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ironwood RPG - Pancake-Scripts
 // @namespace    http://tampermonkey.net/
-// @version      3.0
+// @version      4.0
 // @description  A collection of scripts to enhance Ironwood RPG - https://github.com/Boldy97/ironwood-scripts
 // @author       Pancake
 // @match        https://ironwoodrpg.com/*
