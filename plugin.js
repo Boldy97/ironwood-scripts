ac154467d59c586eb25d80ab23a40c36dfab7d4e
Boldy97 <robin.de.bruin@hotmail.com>
2024-01-15 15:53:04 +0100

    Fix compatibility with GreaseMonkey

--- plugin.js
+++ plugin.js
@@ -7,7 +7,6 @@
 // @match        https://ironwoodrpg.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ironwoodrpg.com
 // @grant        none
-// @run-at       document-body
 // @require      https://code.jquery.com/jquery-3.6.4.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.js
 // ==/UserScript==
