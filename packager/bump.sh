3fe30871415741fdf019af80013babc24780b78d
Boldy97 <robin.de.bruin@hotmail.com>
2024-02-11 00:43:47 +0100

    Update release workflow

--- packager/bump.sh
+++ packager/bump.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git checkout main
+git merge development
 version=$(node.exe bump.js $1)
 node.exe run.js
 git add ../*
@@ -7,5 +9,8 @@ git commit -m "Version $version"
 git tag "v$version"
 git push
 git push origin --tags
+git checkout development
+git merge main
+git push
 
 read -p "Press any key to resume ..."
