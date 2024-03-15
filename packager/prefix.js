0b3338c744dfef40e4594bc090587bd6d93ef975
Boldy97 <robin.de.bruin@hotmail.com>
2024-03-15 22:49:24 +0100

    Version 4.2.6

--- packager/prefix.js
+++ packager/prefix.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ironwood RPG - Pancake-Scripts
 // @namespace    http://tampermonkey.net/
-// @version      4.2.5
+// @version      4.2.6
 // @description  A collection of scripts to enhance Ironwood RPG - https://github.com/Boldy97/ironwood-scripts
 // @author       Pancake
 // @match        https://ironwoodrpg.com/*
@@ -12,4 +12,4 @@
 // ==/UserScript==
 
 window.PANCAKE_ROOT = 'https://iwrpg.vectordungeon.com';
-window.PANCAKE_VERSION = '4.2.5';
+window.PANCAKE_VERSION = '4.2.6';
